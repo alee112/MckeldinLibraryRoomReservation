2d8c46dbde229d511baf207e6c09b656b8383f1a
alee112 <alee112@terpmail.umd.edu>
2016-11-08 17:52:23 -0500

    master

--- room_res/views.py
+++ room_res/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+	# context = context = { 'instructor_list': Instructor.objects.all() }
+ #    return render(request, 'grading/index.html', context)
     return HttpResponse("Hello. This is just a test.")

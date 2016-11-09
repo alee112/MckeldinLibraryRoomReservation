60322faecd154d8b20d0a014da9eb60191039775
Jee Kang <jeekang@vpn-031.umiacs.umd.edu>
2016-11-09 17:54:27 -0500

    master

--- room_res/views.py
+++ room_res/views.py
@@ -11,6 +11,12 @@ def index(request):
 
 #Working on getting field values from html
 #def field_values(request):
-#    room_id = request.getvalue('Room Id')
-#    time = request.getvalue('time')
-#    name = request.getvalue('name')
+#    room = request.GET[Room Id']
+#        if room.is_valid():
+#            room_id = room.cleaned_data['value']
+#    time = request.GET['time']
+#        if time.is_valid():
+#            time_value = time.cleaned_data['value']
+#    name = request.GET['name']
+#        if name.is_valid():
+#            name_value = room.cleaned_data['value']

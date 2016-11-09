02c7ccf55875b762f4734cc83e90ec95e4da22b8
Jee Kang <jeekang@Jees-MacBook-Pro.local>
2016-11-09 15:11:07 -0500

    master

--- room_res/views.py
+++ room_res/views.py
@@ -8,3 +8,9 @@ from django.template import Context
 def index(request):
     context = Context({ 'some_num': 134 })
     return render(request, 'dummy.html', context)
+
+#Working on getting field values from html
+#def field_values(request):
+#    room_id = request.getvalue('Room Id')
+#    time = request.getvalue('time')
+#    name = request.getvalue('name')

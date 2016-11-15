a7c0b26ce118aaebd0e7228539a6c97961036e3a
Andrew Chow <achow101@gmail.com>
2016-11-15 17:47:55 -0500

    URL fixups, gitignore fixup

--- room_res/urls.py
+++ room_res/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
     url(r'^$', views.index, name='index'),
 
     url(r'^test$', views.test, name='test'),
--- room_res/views.py
+++ room_res/views.py
@@ -11,17 +11,15 @@ def index(request):
     if request.method == "POST":
         name = request.POST["Name"]
         context = Context({'name':name})
-        return render(request, 'home.html', context)
+        return render(request, 'index.html', context)
 
     else:
         context = Context({'name':""})
-        return render(request, 'home.html', context)
-def home(request):
-    return render(request, 'home.html')
+        return render(request, 'index.html', context)
 
 def test(request):
 	context = Context({'some_num': 134 })
-	return render(request, 'home.html', context)
+	return render(request, 'dummy.html', context)
 
 #Working on getting field values from html
 #def field_values(request):

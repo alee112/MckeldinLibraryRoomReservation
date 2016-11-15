6d6a15a6f3ae4423974bb21b49782fc6bdbc363a
alee112 <alee112@terpmail.umd.edu>
2016-11-15 17:31:03 -0500

    master

--- room_res/urls.py
+++ room_res/urls.py
@@ -19,4 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'^test$', views.test, name='test'),
 ]
--- room_res/views.py
+++ room_res/views.py
@@ -9,6 +9,10 @@ def index(request):
     context = Context({ 'some_num': 134 })
     return render(request, 'dummy.html', context)
 
+def test(request):
+	context = Context({'some_num': 134 })
+	return render(request, 'home.html', context)
+
 #Working on getting field values from html
 #def field_values(request):
 #    room = request.GET[Room Id']

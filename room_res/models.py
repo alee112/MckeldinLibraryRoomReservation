1fb502e288606e3deaa22923bf52e619f6d6895e
Andrew Chow <achow101@gmail.com>
2017-04-11 14:43:17 -0400

    Fix indentation

--- room_res/models.py
+++ room_res/models.py
@@ -5,20 +5,20 @@ from django.db import models
 # from django.utils import timezone
 
 class Student(models.Model):
-	UID = models.IntegerField(primary_key = True)
-	first_name = models.CharField(max_length = 50)
-	last_name = models.CharField(max_length = 50)
+    UID = models.IntegerField(primary_key = True)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
 
-	def __str__(self):
-		return self.first_name +','+ last_name
+    def __str__(self):
+        return self.first_name +','+ last_name
 
 
-# class Room(models.Model):
-# 	Name = models.CharField(max_length=50)
-# 	def __str__(self):
-# 		return self.name
+    # class Room(models.Model):
+    # 	Name = models.CharField(max_length=50)
+    # 	def __str__(self):
+    # 		return self.name
 
-# class RoomSystem(models.Model):
-# 	Time = models.CharField(max_length=50
-# 	def __str__(self):
-# 		return self.name
+    # class RoomSystem(models.Model):
+    # 	Time = models.CharField(max_length=50
+    # 	def __str__(self):
+    # 		return self.name

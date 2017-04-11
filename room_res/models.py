a317b59048b258b42f9de54600092c4359963571
alee112 <alee112@terpmail.umd.edu>
2017-04-11 13:32:32 -0400

    first commit

--- room_res/models.py
+++ room_res/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+# import datetime
+# from django.utils import timezone
+
+# class Student(models.model):
+# 	UID = models.CharField(max_length=50)
+# 	def __str__(self):
+# 		return self.name
+# class Room(models.Model):
+# 	Name = models.CharField(max_length=50)
+# 	def __str__(self):
+# 		return self.name
+
+# class RoomSystem(models.Model):
+# 	Time = models.CharField(max_length=50
+# 	def __str__(self):
+# 		return self.name
